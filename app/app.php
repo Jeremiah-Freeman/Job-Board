724d8745106da236f745defa20c4476398da2603
jay Freeman and Philip Putnam <jay@>
2017-02-07 08:59:30 -0800

    create initial skeleton files and folders

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+
+
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+        return "hello";
+
+    });
+
+
+
+    return $app;
+?>
